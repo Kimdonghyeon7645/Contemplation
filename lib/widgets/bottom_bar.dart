c352897228a0261525967bbfe3e7224af0dfe249
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2021-05-17 14:59:57 +0900

    code : add BottomBar items

--- lib/widgets/bottom_bar.dart
+++ lib/widgets/bottom_bar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
 class BottomBar extends StatelessWidget {
   @override
@@ -7,7 +8,10 @@ class BottomBar extends StatelessWidget {
       currentIndex: 0,
       items: [
         BottomNavigationBarItem(icon: Icon(Icons.home), label: "홈"),
-        BottomNavigationBarItem(icon: Icon(Icons.widgets_rounded), label: "콘텐츠"),
+        BottomNavigationBarItem(icon: Icon(MdiIcons.recordCircleOutline), label: "명상"),
+        BottomNavigationBarItem(icon: Icon(MdiIcons.tailwind), label: "호흡"),
+        BottomNavigationBarItem(icon: Icon(Icons.person), label: "프로필"),
+        BottomNavigationBarItem(icon: Icon(Icons.widgets_rounded), label: "전체"),
       ],
     );
   }
