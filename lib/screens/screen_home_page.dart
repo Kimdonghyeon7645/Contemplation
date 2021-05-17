24bbe14f8b6b2cd8499d89449ed7df3f57e1dc1f
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2021-05-17 23:53:28 +0900

    code : add HomeContentBox

--- lib/screens/screen_home_page.dart
+++ lib/screens/screen_home_page.dart
@@ -16,16 +16,19 @@ class HomePage extends StatelessWidget {
               floating: true,
               leading: Icon(Icons.menu),
               leadingWidth: Get.width * 0.12,
-              actions: [Padding(
-                padding: EdgeInsets.fromLTRB(0, 0.002 * Get.width, 0.01 * Get.width, 0),
-                child: Icon(MdiIcons.recordCircleOutline),
-              )],
+              actions: [
+                Padding(
+                  padding: EdgeInsets.fromLTRB(
+                      0, 0.002 * Get.width, 0.01 * Get.width, 0),
+                  child: Icon(MdiIcons.recordCircleOutline),
+                )
+              ],
               elevation: 0,
             ),
             SliverToBoxAdapter(
               child: Container(
                 padding: EdgeInsets.fromLTRB(
-                    0.12 * Get.width, 0, 0, 0.07 * Get.width),
+                    0.12 * Get.width, 0, 0, 0.05 * Get.width),
                 alignment: Alignment.bottomLeft,
                 height: Get.height * 0.1,
                 color: Color.fromRGBO(232, 196, 118, 1),
@@ -33,15 +36,57 @@ class HomePage extends StatelessWidget {
                     style: Get.theme.appBarTheme.textTheme.headline2),
               ),
             ),
-            SliverToBoxAdapter(
-              child: Container(
-                height: Get.height * 2,
-                color: Colors.grey.withOpacity(0.1),
-              ),
+            HomeContentBox(
+              title: "지금 기분은 어떠신가요?",
+              subTitle: "현재 상태에 필요한 명상들을 추천해드립니다",
+              captions: ["나태", "혼란", "분노", "탐욕", "불안", "두려움", "슬픔", "힘듬"],
+              images: [
+                "http://images.veritas.kr/data/images/full/22535/painful.jpg",
+                "http://images.veritas.kr/data/images/full/22535/painful.jpg",
+                "http://images.veritas.kr/data/images/full/22535/painful.jpg",
+                "http://images.veritas.kr/data/images/full/22535/painful.jpg",
+                "http://images.veritas.kr/data/images/full/22535/painful.jpg",
+                "http://images.veritas.kr/data/images/full/22535/painful.jpg",
+                "http://images.veritas.kr/data/images/full/22535/painful.jpg",
+                "http://images.veritas.kr/data/images/full/22535/painful.jpg",
+              ],
             )
           ],
         ),
         bottomNavigationBar: BottomBar(),
+      ),
+    );
+  }
+}
+
+class HomeContentBox extends StatelessWidget {
+  final String title, subTitle;
+  final List<String> captions, images;
+
+  HomeContentBox({this.title, this.subTitle, this.captions, this.images});
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverToBoxAdapter(
+      child: Container(
+        color: Get.theme.canvasColor,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: EdgeInsets.fromLTRB(Get.width * 0.05, Get.height * 0.03, 0, 0),
+              child: Text(title, style: Get.theme.textTheme.headline5),
+            ),
+            Padding(
+              padding: EdgeInsets.fromLTRB(Get.width * 0.05, Get.height * 0.003, 0, 0),
+              child: Text(subTitle, style: Get.theme.textTheme.headline6),
+            ),
+            // Container(
+            //   height: Get.height * 0.26,
+            //   color: Colors.grey.withOpacity(0.1),
+            // ),
+          ],
+        ),
       ),
     );
   }
