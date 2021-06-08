aaf5c53e7f048c3fdc4be43f517be2c789200693
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2021-06-08 11:29:47 +0900

    code : add start-button in screen_meditation_group_page.dart

--- lib/screens/meditation/screen_meditation_group_page.dart
+++ lib/screens/meditation/screen_meditation_group_page.dart
@@ -11,71 +11,92 @@ class MeditationGroupPage extends StatelessWidget {
     // final MeditationGroupData meditationGroup = Get.arguments;
     return SafeArea(
       child: Scaffold(
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              SizedBox(
-                height: Get.height * 0.39,
-                child: Stack(
-                  children: [
-                    Container(
-                      width: Get.width,
-                      height: Get.height * 0.35,
-                      decoration: BoxDecoration(
-                        image: DecorationImage(
-                            image: NetworkImage(meditationGroup.imageUrl),
-                            fit: BoxFit.cover),
-                      ),
-                    ),
-                    Container(
-                      width: Get.width,
-                      height: Get.height * 0.35,
-                      decoration: BoxDecoration(
-                        gradient: LinearGradient(
-                          begin: Alignment.topCenter,
-                          end: Alignment.bottomCenter,
-                          colors: [
-                            Colors.black.withOpacity(0),
-                            Colors.black.withOpacity(0.6)
-                          ],
-                        ),
-                      ),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Spacer(),
-                          Text(meditationGroup.title,
-                              style: Get.theme.textTheme.headline3
-                                  .copyWith(color: Colors.white)),
-                          SizedBox(height: Get.height * 0.01),
-                          SizedBox(
-                            width: Get.width * 0.85,
-                            child: Text(meditationGroup.description,
-                                style: TextStyle(
-                                    color: Colors.white.withOpacity(0.6),
-                                    fontSize: Get.height * 0.016),
-                                maxLines: 5,
-                                overflow: TextOverflow.ellipsis),
+        body: Stack(
+          children: [
+            SingleChildScrollView(
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: Get.height * 0.39,
+                    child: Stack(
+                      children: [
+                        Container(
+                          width: Get.width,
+                          height: Get.height * 0.35,
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                                image: NetworkImage(meditationGroup.imageUrl),
+                                fit: BoxFit.cover),
                           ),
-                          SizedBox(height: Get.height * 0.065),
-                        ],
-                      ),
+                        ),
+                        Container(
+                          width: Get.width,
+                          height: Get.height * 0.35,
+                          decoration: BoxDecoration(
+                            gradient: LinearGradient(
+                              begin: Alignment.topCenter,
+                              end: Alignment.bottomCenter,
+                              colors: [
+                                Colors.black.withOpacity(0),
+                                Colors.black.withOpacity(0.6)
+                              ],
+                            ),
+                          ),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Spacer(),
+                              Text(meditationGroup.title,
+                                  style: Get.theme.textTheme.headline3
+                                      .copyWith(color: Colors.white)),
+                              SizedBox(height: Get.height * 0.01),
+                              SizedBox(
+                                width: Get.width * 0.85,
+                                child: Text(meditationGroup.description,
+                                    style: TextStyle(
+                                        color: Colors.white.withOpacity(0.6),
+                                        fontSize: Get.height * 0.016),
+                                    maxLines: 5,
+                                    overflow: TextOverflow.ellipsis),
+                              ),
+                              SizedBox(height: Get.height * 0.065),
+                            ],
+                          ),
+                        ),
+                        IconButton(
+                            icon: Icon(Icons.keyboard_backspace,
+                                size: Get.width * 0.07, color: Colors.white),
+                            onPressed: () => Get.back()),
+                        Positioned(
+                          top: Get.height * 0.315,
+                          left: Get.width * 0.04,
+                          child: MeditationsProgressBox(10, 0),
+                        ),
+                      ],
                     ),
-                    IconButton(
-                        icon: Icon(Icons.keyboard_backspace,
-                            size: Get.width * 0.07, color: Colors.white),
-                        onPressed: () => Get.back()),
-                    Positioned(
-                      top: Get.height * 0.315,
-                      left: Get.width * 0.04,
-                      child: MeditationsProgressBox(10, 0),
-                    ),
-                  ],
+                  ),
+                  for (int i = 0; i < meditationGroup.meditations.length; i++) MeditationGroupTile(meditationGroup.meditations[i]),
+                ],
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: GestureDetector(
+                onTap: () {},
+                child: Container(
+                  width: Get.width * 0.9,
+                  height: Get.height * 0.062,
+                  margin: EdgeInsets.only(bottom: Get.height * 0.02),
+                  decoration: BoxDecoration(
+                    color: Colors.lightBlue,
+                    borderRadius: BorderRadius.all(Radius.circular(12))
+                  ),
+                  alignment: Alignment.center,
+                  child: Text("시작하기", style: TextStyle(color: Colors.white)),
                 ),
               ),
-              for (int i = 0; i < meditationGroup.meditations.length; i++) MeditationGroupTile(meditationGroup.meditations[i]),
-            ],
-          ),
+            )
+          ],
         ),
       ),
     );
