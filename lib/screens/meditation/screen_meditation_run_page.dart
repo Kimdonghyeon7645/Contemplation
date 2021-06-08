b13a6476c6a97dd6edec3a7c77237348a483e9f6
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2021-06-08 16:20:44 +0900

    code : add progressbar

--- lib/screens/meditation/screen_meditation_run_page.dart
+++ lib/screens/meditation/screen_meditation_run_page.dart
@@ -1,5 +1,6 @@
 import 'dart:ui';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -11,50 +12,101 @@ class MeditationRunPage extends StatelessWidget {
         centerTitle: true,
         title: Text("오늘의 추천 명상", style: TextStyle(color: Colors.black)),
         actions: [
-          IconButton(icon: Icon(Icons.bookmark_border), onPressed: () {},),
+          IconButton(icon: Icon(Icons.favorite_border), onPressed: () {},),
         ],
       ),
-      body: Column(
+      body: Stack(
         children: [
-          SizedBox(
-            height: Get.height * 0.51,
-            child: Stack(
-              alignment: Alignment.topCenter,
-              children: [
-                Container(
-                  height: Get.height * 0.46,
-                  width: Get.width,
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                      // image: AssetImage("images/명상02.jpg"),
-                      image: AssetImage("images/daedun.jpg"),
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                  child: BackdropFilter(
-                    filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
-                    child: Container(
-                      decoration: BoxDecoration(color: Colors.black.withOpacity(0.2)),
-                    ),
-                  ),
-                ),
-                Positioned(
-                  top: Get.height * 0.07,
-                  child: Container(
-                    height: Get.height * 0.44,
-                    width: Get.width * 0.8,
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage("images/명상02.jpg"),
+                  // image: AssetImage("images/명상01.jpg"),
+                  fit: BoxFit.cover
+              ),
+            ),
+            child: Container(
+              height: Get.height * 0.62,
+              width: Get.width,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                    Colors.black.withOpacity(0.2),
+                    Colors.black.withOpacity(0),
+                    Colors.black.withOpacity(0.4),
+                  ],
+                )
+              ),
+              child: Column(
+                children: [
+                  SizedBox(height: Get.height * 0.05),
+                  Text("새벽 10분 명상", style: TextStyle(color: Colors.white, fontSize: Get.height * 0.036)),
+                  SizedBox(height: Get.height * 0.006),
+                  Text("일상의 명상", style: TextStyle(color: Colors.white.withOpacity(0.6), fontSize: Get.height * 0.02)),
+                  Spacer(flex: 1),
+                  Container(
+                    height: Get.height * 0.11,
+                    width: Get.height * 0.11,
                     decoration: BoxDecoration(
-                      image: DecorationImage(
-                        image: AssetImage("images/명상02.jpg"),
-                        fit: BoxFit.cover,
-                      ),
-                      borderRadius: BorderRadius.all(Radius.circular(12)),
+                      shape: BoxShape.circle,
+                      color: Colors.white.withOpacity(0.3)
                     ),
-                  )
-                ),
-              ],
+                    child: Icon(Icons.play_arrow, color: Colors.white.withOpacity(0.8), size: Get.height * 0.07),
+                  ),
+                  Spacer(flex: 2),
+                ],
+              ),
             ),
           ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              height: Get.height * 0.32,
+              width: Get.width,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.vertical(top: Radius.circular(30))
+              ),
+              padding: EdgeInsets.all(Get.height * 0.04),
+              child: Column(
+                children: [
+                  Stack(
+                    alignment: Alignment.centerLeft,
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(left: Get.width * 0.01, right: Get.width * 0.01),
+                        child: LinearProgressIndicator(
+                          value: 0.6,
+                          valueColor: AlwaysStoppedAnimation<Color>(Colors.teal),
+                          backgroundColor: Colors.grey.withOpacity(0.2),
+                          minHeight: Get.height * 0.008,
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(left: Get.width * (0.79 - (0.79 * 0.4))),
+                        height: Get.height * 0.018,
+                        width: Get.height * 0.018,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.teal,
+                        ),
+                      )
+                    ],
+                  ),
+                  SizedBox(height: Get.height * 0.01),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text("06:01"),
+                      Text("09:58"),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          )
         ],
       ),
     );
