31aba20ea4531ab966981802fbf93abb2b77cf8d
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2021-06-10 23:53:08 +0900

    code : add breath circular-box in screen_breath_list_page.dart

--- lib/screens/breath/screen_breath_list_page.dart
+++ lib/screens/breath/screen_breath_list_page.dart
@@ -1,5 +1,6 @@
 import 'package:contemplation/widgets/bottom_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class BreathListPage extends StatelessWidget {
   @override
@@ -11,7 +12,50 @@ class BreathListPage extends StatelessWidget {
       ),
       body: Column(
         children: [
-
+          SizedBox(height: Get.height * 0.1),
+          Text("09:10", style: TextStyle(color: Colors.black.withOpacity(0.6), fontSize: Get.height * 0.025)),
+          SizedBox(height: Get.height * 0.014),
+          SizedBox(
+            width: Get.width,
+            child: Stack(
+              alignment: Alignment.center,
+              children: [
+                Container(
+                  height: Get.width * 0.5,
+                  width: Get.width * 0.5,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.grey.withOpacity(0.16),
+                    // border: Border.all(width: Get.height * 0.02, color: Colors.black.withOpacity(0.1)),
+                  ),
+                ),
+                Container(
+                  height: Get.width * 0.2,
+                  width: Get.width * 0.2,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    // color: Colors.redAccent.withOpacity(0.7),
+                    color: Colors.blueAccent.withOpacity(0.7),
+                  ),
+                ),
+                Container(
+                  height: Get.width * 0.4,
+                  width: Get.width * 0.4,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    // color: Colors.redAccent.withOpacity(0.7),
+                    color: Colors.blueAccent.withOpacity(0.5),
+                  ),
+                ),
+                Text("3", style: TextStyle(
+                  fontSize: Get.height * 0.07,
+                  color: Colors.white
+                )),
+              ],
+            ),
+          ),
+          SizedBox(height: Get.height * 0.022),
+          Text("내쉬세요", style: TextStyle(color: Colors.black.withOpacity(0.6), fontSize: Get.height * 0.027)),
         ],
       ),
       bottomNavigationBar: BottomBar(),
