742689ac190bfe8ca16a34b3f3591b12442868ee
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2021-06-15 22:17:32 +0900

    code : apply breath_run_controller.dart

--- lib/controllers/breath_run_controller.dart
+++ lib/controllers/breath_run_controller.dart
@@ -3,17 +3,32 @@ import 'dart:async';
 import 'package:get/get.dart';
 
 class BreathRunController extends GetxController {
+  final List<List<int>> secondList;
   int readySecond = 3;
+  int secondIndex = 0;
+  int showSecond = 0;
+  int breathCount = 0;
   Timer timer;
 
-  @override
-  void onInit() {
-    super.onInit();
+  BreathRunController(List<List<int>> secondList)
+      : this.secondList = secondList {
     timer = Timer.periodic(Duration(seconds: 1), (Timer t) {
-      readySecond--;
-      if (readySecond < 1) {
-        print("죽어라");
-        t.cancel();
+      if (readySecond > 0) {
+        readySecond--;
+      } else {
+        if (secondIndex > secondList.length - 1) {
+          t.cancel();
+        } else if (showSecond > secondList[secondIndex][0]) {
+          breathCount++;
+          showSecond = 1;
+          if (breathCount ~/ 2 > secondList[secondIndex][1]) {
+            breathCount = 0;
+            secondIndex++;
+          }
+        } else {
+          showSecond++;
+        }
+        print("showSecond $showSecond, breathCount $breathCount, secondIndex $secondIndex");
       }
       update();
     });
