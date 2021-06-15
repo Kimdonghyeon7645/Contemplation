346c27ae1db6817e6f50ca35694cfdbe7a7ee1a8
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2021-06-15 16:09:24 +0900

    code : add timer in breath_run_controller.dart

--- lib/controllers/breath_run_controller.dart
+++ lib/controllers/breath_run_controller.dart
@@ -1,5 +1,21 @@
+import 'dart:async';
+
 import 'package:get/get.dart';
 
 class BreathRunController extends GetxController {
+  int readySecond = 3;
+  Timer timer;
 
+  @override
+  void onInit() {
+    super.onInit();
+    timer = Timer.periodic(Duration(seconds: 1), (Timer t) {
+      readySecond--;
+      if (readySecond < 1) {
+        print("죽어라");
+        t.cancel();
+      }
+      update();
+    });
+  }
 }
--- lib/screens/breath/screen_breath_run_page.dart
+++ lib/screens/breath/screen_breath_run_page.dart
@@ -1,70 +1,74 @@
+import 'package:contemplation/controllers/breath_run_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class BreathRunPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    Get.put(BreathRunController());
     return Scaffold(
       appBar: AppBar(
         title:
             Text("8-10초 20분 호흡", style: Get.theme.appBarTheme.titleTextStyle),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text("09:10",
-              style: TextStyle(
-                  color: Colors.black.withOpacity(0.6),
-                  fontSize: Get.height * 0.026,
-                  fontWeight: FontWeight.w400)),
-          SizedBox(height: Get.height * 0.014),
-          SizedBox(
-            width: Get.width,
-            child: Stack(
-              alignment: Alignment.center,
-              children: [
-                Container(
-                  height: Get.width * 0.5,
-                  width: Get.width * 0.5,
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: Colors.grey.withOpacity(0.16),
-                    // border: Border.all(width: Get.height * 0.02, color: Colors.black.withOpacity(0.1)),
+      body: GetBuilder<BreathRunController>(
+        builder: (ctr) => Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("09:10",
+                style: TextStyle(
+                    color: Colors.black.withOpacity(0.6),
+                    fontSize: Get.height * 0.026,
+                    fontWeight: FontWeight.w400)),
+            SizedBox(height: Get.height * 0.014),
+            SizedBox(
+              width: Get.width,
+              child: Stack(
+                alignment: Alignment.center,
+                children: [
+                  Container(
+                    height: Get.width * 0.5,
+                    width: Get.width * 0.5,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.grey.withOpacity(0.16),
+                      // border: Border.all(width: Get.height * 0.02, color: Colors.black.withOpacity(0.1)),
+                    ),
                   ),
-                ),
-                Container(
-                  height: Get.width * 0.21,
-                  width: Get.width * 0.21,
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    // color: Colors.redAccent.withOpacity(0.7),
-                    color: Colors.blueAccent.withOpacity(0.7),
+                  Container(
+                    height: Get.width * 0.21,
+                    width: Get.width * 0.21,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      // color: Colors.redAccent.withOpacity(0.7),
+                      color: Colors.blueAccent.withOpacity(0.7),
+                    ),
                   ),
-                ),
-                Container(
-                  height: Get.width * 0.4,
-                  width: Get.width * 0.4,
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    // color: Colors.redAccent.withOpacity(0.7),
-                    color: Colors.blueAccent.withOpacity(0.5),
+                  Container(
+                    height: Get.width * 0.4,
+                    width: Get.width * 0.4,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      // color: Colors.redAccent.withOpacity(0.7),
+                      color: Colors.blueAccent.withOpacity(0.5),
+                    ),
                   ),
-                ),
-                Text("90",
-                    style: TextStyle(
-                        fontSize: Get.height * 0.055,
-                        fontWeight: FontWeight.w300)),
-              ],
+                  Text(ctr.readySecond.toString() ?? 3,
+                      style: TextStyle(
+                          fontSize: Get.height * 0.055,
+                          fontWeight: FontWeight.w300)),
+                ],
+              ),
             ),
-          ),
-          SizedBox(height: Get.height * 0.022),
-          Text("내쉬세요",
-              style: TextStyle(
-                  color: Colors.black.withOpacity(0.6),
-                  fontSize: Get.height * 0.026,
-                  fontWeight: FontWeight.w300)),
-          SizedBox(height: Get.height * 0.1),
-        ],
+            SizedBox(height: Get.height * 0.022),
+            Text(ctr.readySecond > 0 ? "호흡을 준비해주세요" : " 내쉬세요 ",
+                style: TextStyle(
+                    color: Colors.black.withOpacity(0.6),
+                    fontSize: Get.height * 0.026,
+                    fontWeight: FontWeight.w300)),
+            SizedBox(height: Get.height * 0.1),
+          ],
+        ),
       ),
     );
   }
