c60f56c9c45a0393fe09f2524ca2b6c25e80f796
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2021-06-16 00:00:47 +0900

    code : edit style in screen_record_page.dart

--- lib/screens/record/screen_record_page.dart
+++ lib/screens/record/screen_record_page.dart
@@ -1,6 +1,7 @@
 import 'package:contemplation/controllers/record_controller.dart';
 import 'package:contemplation/widgets/bottom_bar.dart';
 import 'package:contemplation/widgets/record/calendar_box.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -9,18 +10,70 @@ class RecordPage extends StatelessWidget {
   Widget build(BuildContext context) {
     Get.put(RecordController());
     return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "기록",
+          style: Get.theme.appBarTheme.titleTextStyle,
+        ),
+      ),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          SizedBox(height: Get.height * 0.04),
+          SizedBox(height: Get.height * 0.02),
+          Padding(
+            padding: EdgeInsets.only(left: Get.width * 0.06),
+            child: Text(
+              "현재 레벨",
+              style: TextStyle(
+                  fontWeight: FontWeight.w800, fontSize: Get.height * 0.022),
+            ),
+          ),
+          SizedBox(height: Get.height * 0.02),
+          Row(
+            children: [
+              Spacer(),
+              Text("레벨",
+                  style: TextStyle(
+                      fontSize: Get.height * 0.02,
+                      fontWeight: FontWeight.w200)),
+              SizedBox(width: Get.width * 0.03),
+              Text("1",
+                  style: TextStyle(
+                      color: Colors.indigo,
+                      fontSize: Get.height * 0.06,
+                      fontWeight: FontWeight.w500)),
+              SizedBox(width: Get.width * 0.01),
+              Text("LV",
+                  style: TextStyle(
+                      color: Colors.indigo, fontSize: Get.height * 0.016)),
+              Spacer(),
+              Text("경험치"),
+              SizedBox(width: Get.width * 0.03),
+              Text("0",
+                  style: TextStyle(
+                      color: Colors.indigo,
+                      fontSize: Get.height * 0.06,
+                      fontWeight: FontWeight.w500)),
+              SizedBox(width: Get.width * 0.01),
+              Text("exp",
+                  style: TextStyle(
+                      color: Colors.indigo, fontSize: Get.height * 0.016)),
+              Spacer(),
+            ],
+          ),
+          SizedBox(height: Get.height * 0.05),
           GetBuilder<RecordController>(
             builder: (ctr) => Row(
               children: [
                 SizedBox(width: Get.width * 0.06),
+                Text("날짜별 기록", style: TextStyle(fontSize: Get.height * 0.022, fontWeight: FontWeight.w800)),
+                Spacer(),
                 Text("${ctr.currentViewMonth}월",
-                    style: TextStyle(fontSize: Get.height * 0.034)),
+                    style: TextStyle(fontSize: Get.height * 0.026)),
                 SizedBox(width: Get.width * 0.02),
                 Text("${ctr.currentViewYear}년",
-                    style: TextStyle(fontSize: Get.height * 0.02)),
+                    style: TextStyle(fontSize: Get.height * 0.018)),
+                SizedBox(width: Get.width * 0.05),
               ],
             ),
           ),
@@ -30,11 +83,11 @@ class RecordPage extends StatelessWidget {
             child: PageView(
               children: [
                 for (int number =
-                    Get.find<RecordController>().leftmostMotionYear;
-                number < Get.find<RecordController>().rightmostMotionYear;
-                number = (number % 100 > 11)
-                    ? (number ~/ 100 + 1) * 100 + 2
-                    : number + 1)
+                        Get.find<RecordController>().leftmostMotionYear;
+                    number < Get.find<RecordController>().rightmostMotionYear;
+                    number = (number % 100 > 11)
+                        ? (number ~/ 100 + 1) * 100 + 2
+                        : number + 1)
                   CalendarBox(number, MODE.DAY)
               ],
               onPageChanged: (int index) =>
@@ -43,6 +96,7 @@ class RecordPage extends StatelessWidget {
                   initialPage: Get.find<RecordController>().startIndex),
             ),
           ),
+          SizedBox(height: Get.height * 0.02),
         ],
       ),
       bottomNavigationBar: BottomBar(),
