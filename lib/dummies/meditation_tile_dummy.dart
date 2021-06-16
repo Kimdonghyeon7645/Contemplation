3727c9ccc786322accd163d4464d233fa241d6a4
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2021-06-16 15:10:25 +0900

    code : edit meditation_group_dummy.dart

--- lib/dummies/meditation_tile_dummy.dart
+++ lib/dummies/meditation_tile_dummy.dart
@@ -3,37 +3,37 @@ import 'package:contemplation/models/meditation_list_tile.dart';
 List<MeditationTile> meditationTiles = [
   MeditationTile.fromMap({
     "name": "매일매일 10분 명상",
-    "imageUrl": "https://post-phinf.pstatic.net/MjAxODEwMTZfMTYx/MDAxNTM5NjQ4NDk0NDgy.aFtMBFpBAn6w4L5eI0gt90lUpf-PRrAu1gYg71Fz2Vsg.fuxPmMjvPF4Rt1Z5pLPnqqsWjRAJESIb3QRTqhD25fgg.JPEG/twi001t235887_l.jpg?type=w1200",
+    "imageUrl": "images/명상07.png",
     "length": 2,
   }),
   MeditationTile.fromMap({
     "name": "직관과 함께하는 명상",
-    "imageUrl": "https://pds.joins.com/news/component/htmlphoto_mmdata/201909/23/b8abc429-54aa-46a2-bc22-f66a42ff95a9.jpg",
+    "imageUrl": "images/명상08.jpg",
     "length": 7,
   }),
   MeditationTile.fromMap({
     "name": "몰입 명상",
-    "imageUrl": "https://wonderfulmind.co.kr/wp-content/uploads/2017/03/girl-meditating-600x393.jpg",
+    "imageUrl": "images/daedunsan.jpg",
     "length": 4,
   }),
   MeditationTile.fromMap({
     "name": "지혜를 개발하는 명상",
-    "imageUrl": "https://t1.daumcdn.net/cfile/tistory/252D9B3F549283D10D",
+    "imageUrl": "images/명상03.jpg",
     "length": 5,
   }),
   MeditationTile.fromMap({
     "name": "자연소리와 함께하는 명상",
-    "imageUrl": "https://www.bulkwang.co.kr/news/photo/201909/34235_8569_5127.jpg",
+    "imageUrl": "images/명상06.jpg",
     "length": 7,
   }),
   MeditationTile.fromMap({
     "name": "치유와 자명의 명상",
-    "imageUrl": "https://image.freepik.com/free-vector/monk-meditation_11700-320.jpg",
+    "imageUrl": "images/연꽃01.jpg",
     "length": 2,
   }),
   MeditationTile.fromMap({
     "name": "고전과 함께하는 명상",
-    "imageUrl": "https://img.sbs.co.kr/newimg/news/20200825/201464715_1280.jpg",
+    "imageUrl": "images/홍익01.jpg",
     "length": 12,
   }),
 ];
--- lib/screens/meditation/screen_meditation_group_page.dart
+++ lib/screens/meditation/screen_meditation_group_page.dart
@@ -1,4 +1,5 @@
 import 'package:contemplation/dummies/meditation_group_dummy.dart';
+
 // import 'package:contemplation/models/meditation_group.dart';
 import 'package:contemplation/widgets/meditation/meditation_group_tile.dart';
 import 'package:contemplation/widgets/meditation/meditations_progress_box.dart';
@@ -25,7 +26,12 @@ class MeditationGroupPage extends StatelessWidget {
                           height: Get.height * 0.35,
                           decoration: BoxDecoration(
                             image: DecorationImage(
-                                image: NetworkImage(meditationGroup.imageUrl),
+                                image: meditationGroup.imageUrl
+                                            .split("/")
+                                            .first ==
+                                        "images"
+                                    ? AssetImage(meditationGroup.imageUrl)
+                                    : NetworkImage(meditationGroup.imageUrl),
                                 fit: BoxFit.cover),
                           ),
                         ),
@@ -75,22 +81,23 @@ class MeditationGroupPage extends StatelessWidget {
                       ],
                     ),
                   ),
-                  for (int i = 0; i < meditationGroup.meditations.length; i++) MeditationGroupTile(meditationGroup.meditations[i]),
+                  for (int i = 0; i < meditationGroup.meditations.length; i++)
+                    MeditationGroupTile(meditationGroup.meditations[i]),
                 ],
               ),
             ),
             Align(
               alignment: Alignment.bottomCenter,
               child: GestureDetector(
-                onTap: () => Get.toNamed("/meditation", arguments: meditationGroup.meditations.first),
+                onTap: () => Get.toNamed("/meditation",
+                    arguments: meditationGroup.meditations.first),
                 child: Container(
                   width: Get.width * 0.9,
                   height: Get.height * 0.062,
                   margin: EdgeInsets.only(bottom: Get.height * 0.02),
                   decoration: BoxDecoration(
-                    color: Colors.indigo,
-                    borderRadius: BorderRadius.all(Radius.circular(12))
-                  ),
+                      color: Colors.indigo,
+                      borderRadius: BorderRadius.all(Radius.circular(12))),
                   alignment: Alignment.center,
                   child: Text("시작하기", style: TextStyle(color: Colors.white)),
                 ),
