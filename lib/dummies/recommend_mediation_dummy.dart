1d11f287f967bda4e493bf5813012f499e4b221e
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2021-06-16 15:27:47 +0900

    code : add recommend_mediation_dummy.dart

---
+++ lib/dummies/recommend_mediation_dummy.dart
@@ -0,0 +1,32 @@
+import 'package:contemplation/models/meditation_group.dart';
+
+List<MeditationGroupTileData> recommendMeditationList = [
+  MeditationGroupTileData.fromMap({
+    "title": "고뇌를 잊는 명상",
+    "groupName": "일상 속의 명상",
+    "imageUrl": "https://t1.daumcdn.net/cfile/tistory/99CACA455CC49E2105",
+    "second": 300,
+    "isCompleted": false,
+  }),
+  MeditationGroupTileData.fromMap({
+    "title": "오직 모를뿐 명상",
+    "groupName": "자명을 깨치는 명상",
+    "imageUrl": "images/명상09.jpg",
+    "second": 300,
+    "isCompleted": false,
+  }),
+  MeditationGroupTileData.fromMap({
+    "title": "마음을 비우는 명상",
+    "groupName": "일상 속의 명상",
+    "imageUrl": "images/명상01.jpg",
+    "second": 400,
+    "isCompleted": false,
+  }),
+  MeditationGroupTileData.fromMap({
+    "title": "두통이 있을 때 명상",
+    "groupName": "일상 속의 명상",
+    "imageUrl": "https://lh3.googleusercontent.com/proxy/SGimXMyq_YXjrPZ4652aZ6s0C53Cmp-J-0wF4Y_nezGRmX-c9sCsM5IlusBEpVBjIxZlCVFV6qUurXnz_lnxt3TfwqMsdTqKmhiTP7UcUslBmfKnc0jjWWHBYArZflzrXxX-PBQzxZ3pre13npUfAfPqlxiu",
+    "second": 600,
+    "isCompleted": false,
+  }),
+];
